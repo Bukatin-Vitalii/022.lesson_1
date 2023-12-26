68278530151786af09d8621143009bb9b14b21fe
Vitalii Bukatin <bukatin93@gmail.com>
2023-12-26 12:35:28 +0100

    completed hw

---
+++ index.js
@@ -0,0 +1,12 @@
+import add from './operations/add.js';
+import sub from './operations/sub.js';
+import div from './operations/div.js';
+import mult from './operations/mult.js';
+
+const a = 10;
+const b = 5;
+
+console.log('Addition:', add(a, b));
+console.log('Subtraction:', sub(a, b));
+console.log('Division:', div(a, b));
+console.log('Multiplication:', mult(a, b));
